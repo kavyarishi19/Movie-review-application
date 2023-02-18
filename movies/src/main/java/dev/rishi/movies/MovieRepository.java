42a6eeb6f1a00f6c9a58444994bf48501f73270e
kavyarishi19 <forcodingpurposes19@gmail.com>
2023-02-18 17:04:04 +0530

    added feature of getting all movies

---
+++ movies/src/main/java/dev/rishi/movies/MovieRepository.java
@@ -0,0 +1,7 @@
+package dev.rishi.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MovieRepository extends MongoRepository<Movies , ObjectId> {
+}
