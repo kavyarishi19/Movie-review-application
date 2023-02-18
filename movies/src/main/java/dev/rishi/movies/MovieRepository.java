b66a24f45a37ce2debf5911904335109cf69dafa
kavyarishi19 <forcodingpurposes19@gmail.com>
2023-02-18 23:29:57 +0530

    added finding by imdb id

--- movies/src/main/java/dev/rishi/movies/MovieRepository.java
+++ movies/src/main/java/dev/rishi/movies/MovieRepository.java
@@ -3,5 +3,9 @@ package dev.rishi.movies;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface MovieRepository extends MongoRepository<Movies , ObjectId> {
+
+    Optional<Movies> findMovieByImdbId(String imdbId);
 }
